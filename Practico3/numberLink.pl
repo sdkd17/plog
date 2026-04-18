c8b209ec35da60f21cc23117664d4c47c3632c54
sdkd17 <sdkd17@gmail.com>
2026-04-18 17:20:53 -0300

    numberLink resuelve ejemplo de letra

--- Practico3/numberLink.pl
+++ Practico3/numberLink.pl
@@ -1,40 +1,52 @@
 
 generar_pares(N,(I,J)) :-
-  N1 is N - 1,
-  between(0,N1,I),
-  between(0,N1,J).
+  N1 is N,
+  between(1,N1,I),
+  between(1,N1,J).
 
-adyacente((I1, J1), (I1, J2), N) :- J2 is J1 + 1, J1 < N-1.
-adyacente((I1, J1), (I1, J2), N) :- J2 is J1 - 1, J1 > 0.
-adyacente((I1, J1), (I2, J1), N) :- I2 is I1 + 1, I1 < N-1.
-adyacente((I1, J1), (I2, J1), N) :- I2 is I1 - 1, I1 > 0.
+adyacente((I1, J1), (I1, J2), N) :- J2 is J1 + 1, J1 < N.
+adyacente((I1, J1), (I1, J2), N) :- J2 is J1 - 1, J1 > 1.
+adyacente((I1, J1), (I2, J1), N) :- I2 is I1 + 1, I1 < N.
+adyacente((I1, J1), (I2, J1), N) :- I2 is I1 - 1, I1 > 1.
 
 %todos los caminos posibles de Ini a Fin en un tablero de tamanio N
-caminos(N, Ini, Fin, C, L) :- caminos_acc(Ini,Fin, N, [], C, 0, L).
+caminos(N, Ini, Fin, C, L, Visitados) :- caminos_acc(Ini,Fin, N, [Ini], C, 1, L, Visitados).
 
-caminos_acc((I1,J1),(I1,J1),_,R,R,RLenghtAcc,RLenghtAcc).
+caminos_acc(X,X,_,Visitados,Visitados,RLenghtAcc,RLenghtAcc, _).
 
-caminos_acc((I1,J1),(I2,J2),N,Visitados,R,RLenghtAcc, RLength) :-  
-  generar_pares(N,(X1,X2)),
-  \+ member((X1,X2), Visitados),
-  adyacente((I1,J1),(X1,X2),N),
+caminos_acc(I,F,N,Visitados,R,RLenghtAcc, RLength, VisitadosPorOtros) :-  
+  generar_pares(N,P),
+  \+ member(P, Visitados),
+  \+ member(P, VisitadosPorOtros),
+  adyacente(I,P,N),
   RLenght1 is RLenghtAcc + 1,
-  caminos_acc((X1,X2),(I2,J2),N,[(X1,X2)|Visitados],R, RLenght1, RLength).
+  caminos_acc(P,F,N,[P|Visitados],R, RLenght1, RLength, VisitadosPorOtros).
+
+  
+numberLink(N,Puntos,C) :- numberLink_acc(N,Puntos,[],C,[],0).
 
 % N tamanio del tablero
 % Lista de Pares inicio fin
 % Caminos Acumulador caminos generados hasta el momento
-% CaminosLength Largo de Caminos para validar que cubro todo el tablero
+% CaminosLength suma de Largo de Caminos para validar que cubro todo el tablero
 % Visitados Acumulador Para validar que no se crucen los caminos
 
-numberLink(N,[],Caminos,CaminosLength,_,Caminos) :- CaminosLength is (N*N) - 1. %Si visite todos los nodos llego a una solucion.
+numberLink_acc(N,[],Caminos,Caminos,_,VisitadosLength) 
+  % :- write(VisitadosLength). 
+  :- VisitadosLength is (N*N).
 
-numberLink(N,[(Ini,Fin)|Ps], Caminos, CaminosLength, Visitados, C) :-
-  caminos(N,Ini,Fin,R,RLength), %Busco un camino entre Ini y Fin
+numberLink_acc(N,[(Ini,Fin)|Ps], Caminos, C, Visitados, VisitadosLength) :-
+  caminos(N,Ini,Fin,R,RLength, Visitados), %Busco un camino entre Ini y Fin, queda en R
+  % write(RLength),
+  % writeq('Caminos=>'),write(Ini),writeq('-'),write(Fin),writeln(Caminos),
+  % writeq('R=>'),writeln(R),
+  % writeq('visitados=>'),writeln(Visitados),
+  
+  \+ intersect(R,Visitados), %El nuevo camino no se cruza con otro nodo ya visitado
   append(R, Visitados, VisitadosAcc), % 
-  CaminosLength1 is CaminosLength + RLength,
-  \+ intersect(R,VisitadosAcc), %El nuevo camino no se cruza con otro nodo ya visitado
-  numberLink(N, Ps, [R|Caminos], CaminosLength1, VisitadosAcc,C).
+  VisitadosLength1 is VisitadosLength + RLength,
+  
+  numberLink_acc(N, Ps, [R|Caminos], C, VisitadosAcc, VisitadosLength1).
   
 %interseccion de conjuntos/listas  
 intersect(L1,L2) :-
