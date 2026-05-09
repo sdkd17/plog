fbdca8be8f689adee616da0404e8379c9021627b
sdkd17 <sdkd17@gmail.com>
2026-05-09 18:24:14 -0300

    previa lab2

---
+++ lab2/insumos/ginrummylog.pl
@@ -0,0 +1,13 @@
+% is_meld(+Cartas) -  Se cumple si y solo si Cartas es un set o un run válido
+
+% valor_deadwood(+Cartas, ?Valor) - Valor es la suma de valores de puntos de las cartas en Cartas según la tabla de deadwood.
+
+% get_melds(+Mano, ?Melds, ?Sobrantes)
+
+% best_melds(+Mano, ?MejorMelds, ?Sobrante, ?Valor)
+
+% robar(+Mano, +Descarte, +CartasVistas, +Estrategia, ?Lugar)
+
+% descartar(+OldMano, +CartasVistas, +Estrategia, ?NewMano, ?NewDescarte)
+
+% cerrar(+Mano, +CartasVistas, +Estrategia, ?Decision)
