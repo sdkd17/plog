9da107a124315adbb1209b189453e3351d1be880
sdkd17 <sdkd17@gmail.com>
2026-07-03 16:28:43 -0300

    lab1

--- lab1/lab1.pl
+++ lab1/lab1.pl
@@ -1,282 +1,174 @@
-% pertenece(?X,?L) <- El elemento X pertenece a la lista L
+%SERGIO DANIEL KLEIN DURAN
+%JOAQUIN GUANI SANZ
+%LUKAS BERNARD KASEK ANTUNEZ
+%GUILLERMO MAXIMILIANO FRANCO SUNES
 
-
-/*
-Ejemplos:
-    ?- pertenece(a,[a,b,c]).
-    true .
-    
-    ?- pertenece(X,[a,b,c]). X=a;
-    X=b;
-    X=c;
-    false.
-*/
-
+%pertenece(?X,?L) ← Devuelve true si el elemento X pertenece a la lista L
 pertenece(X,[X|_]).
-pertenece(X, [_|Ls]) :- pertenece(X,Ls).
+pertenece(X, [Y|Ls]) :- X \== Y, pertenece(X,Ls).
 
-% seleccionar(?X,?L,?L1) <- La lista L1 es el resultado de eliminar una ocurrencia del elemento X de la lista L.
-
-/*
- Ejemplos:
-    ?- seleccionar(3,[1,3,5,3], L).
-    L = [1, 5, 3] ;
-    L = [1, 3, 5] ;
-    false.
-
-    ?- seleccionar(3,L,[1,2]).
-    L = [3, 1, 2] ;
-    L = [1, 3, 2] ;
-    L = [1, 2, 3].
-*/
-
-% seleccionar(_, [], []).
-% seleccionar(X, [X|Ls], Ls).
-% seleccionar(X, [L|Ls], L1) :- seleccionar(X, Ls, L2),  append([L], L2, L1).
-
-seleccionar(_, [], []).
+%seleccionar(?X,?L,?L1) ← La lista L1 es el resultado de eliminar una ocurrencia del elemento X de la lista L.
 seleccionar(X, [X|Ls], Ls).
 seleccionar(X, [Y|Ls], [Y|L1]) :- seleccionar(X, Ls, L1).
 
-% rotar(+L,+N,-R) <- La lista R es el resultado de rotar la lista L N veces hacia la izquierda. Si N es negativo, el movimiento será hacia la derecha. N puede ser mayor al tamaño de la lista. 
+/*dividir(+L, +N, -A, -B) ← La lista A contiene los primeros N elementos de L y la lista B los restantes de L.*/
+dividir(L, 0, [], L).
+dividir([H|T], N, [H|A], B) :-
+    N > 0,
+    N1 is N - 1,
+    dividir(T, N1, A, B).
 
-/*Ejemplos:
-    ?- rotar([1,2,3,4,5],1,R).
-    R = [2, 3, 4, 5, 1].
-  
-    ?- rotar([1,2,3,4,5],-1,R).
-    R = [5, 1, 2, 3, 4].
-  
-    ?- rotar([1,2,3,4,5],8,R).
-    R = [4, 5, 1, 2, 3].
-*/
+/*rotar(+L,+N,-R) ← La lista R es el resultado de rotar la lista L N veces hacia la izquierda.
+Si N es negativo, el movimiento será hacia la derecha. N puede ser mayor al tamaño de la
+lista.*/
+rotar(L,0,L).
+rotar(L, N, Resultado) :-
+    length(L, Largo),
+    Largo > 0,
+    N1 is N mod Largo,          
+    dividir(L, N1, A, B),
+    append(B, A, Resultado).
 
-rotar(L,N,R) :- 
-    reverso(L,Lrev,Largo), %invierto la lista y obtengo largo al mismo tiempo
-    M is N mod Largo,
-    primeros_n_rev(Lrev,M,P), % primera parte del resultado
-    M1 is Largo - M,
-    primeros_n(L,M1,U), % segunda parte del resultado
-    append(P,U,R). %concateno primera y segunda parte
-
-% reverso(+L,?R,?Largo) <- Retorna en R la lista L invertida y retorna en Largo el largo de la lista
-
-/* Ejemplo:
-    ?- reverso([1,2,3,4,5], R, Largo).
-    R = [5,4,3,2,1],
-    Largo = 5.
-*/
-reverso(L,R,Largo) :- reverso_acc(L,[],R, Largo, 0).
-
-reverso_acc([],Acc,Acc, LargoAcc, LargoAcc).
-reverso_acc([X|Xs],Acc,R, Largo, LargoAcc) :-
-    LargoAcc1 is LargoAcc + 1,
-    reverso_acc(Xs,[X|Acc],R, Largo, LargoAcc1).
-
-% primeros_n_rev(+L,+N,?R) <- devuelve en R la lista de los primeros N elementos de la lista L en orden inverso
-
-/*Ejemplos:
-    ?- primeros([1,2,3,4,5],3,R).
-    R = [3,2,1].
-*/
-primeros_n_rev(L,N,R) :- primeros_n_rev_acc(L,0,N,[],R).
-
-primeros_n_rev_acc(_,N,N,AccL,AccL).
-primeros_n_rev_acc([L|Ls],AccN,N, AccL, R) :-
-    AccN1 is AccN + 1,
-    primeros_n_rev_acc(Ls, AccN1, N, [L|AccL], R).
-
-%% primeros_n(+L,+N,?R) <- devuelve en R la lista de los primeros N elementos de la lista L
-
-/*Ejemplos:
-    ?- primeros([1,2,3,4,5],3,R).
-    R = [1,2,3].
-*/
-primeros_n(L,N,R) :- primeros_n_acc(L,0,N,R).
-
-primeros_n_acc(_,N,N,[]).
-primeros_n_acc([L|Ls],AccN,N,[L|R]) :-
-    AccN1 is AccN + 1,
-    primeros_n_acc(Ls, AccN1, N, R).
-
-% rotar_varias(+L, +Pares,-R) <- dada una Lista L, y una lista Pares de pares (N,Dir), la lista R es el resultado de aplicar una rotación por cada par, donde N indica la cantidad de posiciones, y Dir puede ser izquierda o derecha. 
-/* Ejemplo:
-    ?- rotar_varias([1,2,3,4,5],[(2, izquierda), (1, derecha), (3, izquierda)],R).
-    R = [5, 1, 2, 3, 4].
-*/
-
+/*rotar_varias(+L, +Pares,-R) ← dada una Lista L, y una lista Pares de pares (N,Dir),
+la lista R es el resultado de aplicar una rotación por cada par, donde N indica la cantidad de
+posiciones, y Dir puede ser izquierda o derecha.*/
 rotar_varias(L, [], L).
-rotar_varias(L,[P|Ps], R) :- 
-    rotar_una(L,P,L1), 
-    rotar_varias(L1, Ps, R).
+rotar_varias(L, [(N, izquierda)|T], R) :-
+    rotar(L, N, L1),
+    rotar_varias(L1, T, R).
+rotar_varias(L, [(N, derecha)|T], R) :-
+    N1 is -N,
+    rotar(L, N1, L1),
+    rotar_varias(L1, T, R).
 
-% rotar_una(L, (N,Dir), R) <- dada una Lista L, y un Par (N,Dir), la lista R es el resultado de aplicar una rotación a L, donde N indica la cantidad de posiciones, y Dir puede ser izquierda o derecha. 
-rotar_una(L, (N,izquierda), R) :- 
-    N1 is -1*N, 
-    rotar(L,N1,R).
-rotar_una(L, (N,derecha), R) :- 
-    rotar(L,N,R). 
+/*producto_escalar(+A, +B, -R). ← R es el resultado de realizar la suma del producto entrada a entrada de las listas A y B.*/
+producto_escalar([], [], 0).
+producto_escalar([Ai|F], [Bj|C], Resultado):- producto_escalar(F, C, RecRes), Resultado is Ai * Bj + RecRes.  
 
-% multiplicar_matrices (+A,+B,-R) <- Dadas dos matrices A y B, representadas como listas de listas de números, R es el resultado de multiplicar ambas matrices. De acuerdo a las reglas de multiplicación de matrices, la cantidad de columnas de la matriz A debe ser igual a la cantidad de filas de la matriz B. 
+/*separar_cabezas(+M, -Col, -Resto). ← dada una matriz M, devuelve la primer columna de M en Col y la matriz restante en Resto.*/
+separar_cabezas([], [], []).
+separar_cabezas([[E|Cola] | Filas], [E|Entradas], [Cola|Restos]) :-
+    separar_cabezas(Filas, Entradas, Restos).
 
-/* Ejemplo:
-    ?- multiplicar_matrices([[1,2,3],[4,5,6]], [[1,2],[3,4],[5,6]], R).
-    R = [[22, 28], [49, 64]].
-*/
-multiplicar_matrices(A,B,R) :- multiplicar_matrices_acc(A,B,Rx,[]), reverso(Rx,R,_).
+/*trasponer(+M, -MT). ← dada una matriz M, la matriz MT es el resultado de trasponer M.*/
+trasponer([[]|_], []).
+trasponer([[F1|RestoFila] | FilasRestantes], [FilaTraspuesta | MatrizRestante]) :-
+    separar_cabezas([[F1|RestoFila] | FilasRestantes], FilaTraspuesta, Restos),
+    trasponer(Restos, MatrizRestante).
 
+/*fila_por_matriz(+F, +M, -P). ← dada un array F y una matriz M, P es el resultado de realizar el producto de F con cada fila de M.*/
+fila_por_matriz(_,[],[]).
+fila_por_matriz(F,[BTraspuesta1| Resto],[P|Producto]):-producto_escalar(F,BTraspuesta1,P), fila_por_matriz(F, Resto, Producto).
 
-multiplicar_matrices_acc([],_,Acc,Acc).
-multiplicar_matrices_acc([As|Ass], B, R, Acc) :-
-    multiplicar_fila_x_matriz(As, B, Ri),
-    multiplicar_matrices_acc(Ass, B, R, [Ri|Acc]).
+/*matriz_por_matriz(+A, +B, -R). ← dada las matrices A y B, R es el resultado de realizar el producto escalar entre las filas de A por las filas de B*/
+matriz_por_matriz([], _, []).
+matriz_por_matriz([A1|FilasRA], BTraspuesta, [R1|Restantes]):- fila_por_matriz(A1,BTraspuesta,R1), 
+    matriz_por_matriz(FilasRA, BTraspuesta, Restantes).
 
-multiplicar_fila_x_matriz(Ai, [Bs|Bss], R) :- 
-    multiplicar_fila_x_matriz_acc(Ai, Bs, [Bs|Bss],0, Ri, []),
-    reverso(Ri,R,_).
-
-multiplicar_fila_x_matriz_acc(_,[],_,_,Acc,Acc).
-multiplicar_fila_x_matriz_acc(Ai, [_|Bis], B, I, Ri, Acc) :- 
-    obtener_columna(B, I, Bj),
-    multiplicar_arrays(Ai, Bj, Rij),
-    ISucc is I + 1,
-    multiplicar_fila_x_matriz_acc(Ai, Bis, B, ISucc, Ri, [Rij|Acc]).
-
-obtener_columna(B, Index, Rs) :- 
-    obtener_columna_acc(B,Index, R1, []),
-    reverso(R1,Rs,_).
-
-obtener_columna_acc([], _,Acc,Acc).
-obtener_columna_acc([Bs|Bss], Index, Rs, Acc) :-
-    obtener_columna_acc(Bss, Index, Rs, [Bi|Acc]),
-    obtener_i(Bs, Index, Bi).
-    
-
-obtener_i(L,Index,R) :- obtener_i_acc(L, Index, R, 0).
-
-obtener_i_acc([L|_], Index, L, Index).
-obtener_i_acc([_|Ls],Index, R, Acc) :-
-    Acc1 is Acc + 1,
-    obtener_i_acc(Ls, Index, R, Acc1).
+/*multiplicar_matrices (+A,+B,-R) ← Dadas dos matrices A y B, representadas como
+listas de listas de números, R es el resultado de multiplicar ambas matrices. De acuerdo a las
+reglas de multiplicación de matrices, la cantidad de columnas de la matriz A debe ser igual a
+la cantidad de filas de la matriz B. */
+multiplicar_matrices(A,B,R):-trasponer(B,BTraspuesta), matriz_por_matriz(A,BTraspuesta,R).
 
 
-multiplicar_arrays([],[],0).
-multiplicar_arrays([A|As], [B|Bs], R) :-
-    multiplicar_arrays(As,Bs, R1),
-    R is A*B + R1.
+/* numberlink(+N,+Puntos,-C) ← Si N es un número que representa el tamaño del tablero, y
+Puntos una lista de pares (Inicial,Final), siendo Inicial y Final a su vez pares
+(Fila,Columna), que indican posiciones en el tablero. entonces C será una lista de caminos
+que resuelve el problema. Cada camino será a su vez una lista de pares (Fila,Columna)
+que comienza en el primer componente de cada elemento de Puntos, y termina en el
+segundo. */
+
+numberlink(N, Puntos, C) :-
+    generarCasillasDesde(1, 1, N, Puntos, Casillas),
+    porCasillasPrimero(Casillas, Puntos, C).
 
 
-/*
-  Si N es un número que representa el tamaño del tablero, y Puntos una lista de pares (Inicial,Final),
-  siendo Inicial y Final a su vez pares (Fila,Columna), que indican posiciones en el tablero. 
-  entonces C será una lista de caminos que resuelve el problema. Cada camino será a su vez 
-  una lista de pares (Fila,Columna) que comienza en el primer componente de cada elemento de Puntos, 
-  y termina en el segundo. Por convención, las filas del tablero están numeradas de arriba hacia abajo, 
-  y las columnas de izquierda a derecha. Los caminos deben devolverse en el mismo orden que los 
-  puntos que lo generan.
-*/
-numberLink(N,Puntos,C) :- numberLink_acc(N,Puntos,Puntos,[],C1,[],0), reverso(C1,C,_).
+/* generarCasillasDesde(+F, +C, +N, +Puntos, -Casillas) ← 
+Genera en Casillas todas las posiciones del tablero N x N que no
+están ocupadas por Puntos, comenzando en la fila F y columna C.
+Así, dados F, C y N, solo hay una solución posible.*/
+
+generarCasillasDesde(F, _, N, _, []) :-
+    F > N.
+
+generarCasillasDesde(F, C, N, Puntos, Casillas) :-
+    F =< N,
+    C > N,
+    F1 is F + 1,
+    generarCasillasDesde(F1, 1, N, Puntos, Casillas).
+
+generarCasillasDesde(F, C, N, Puntos, [(F, C) | Resto]) :-
+    F =< N,
+    C =< N,
+    noPerteneceAPunto((F, C), Puntos),
+    C1 is C + 1,
+    generarCasillasDesde(F, C1, N, Puntos, Resto).
+
+generarCasillasDesde(F, C, N, Puntos, Resto) :-
+    F =< N,
+    C =< N,
+    perteneceAPunto((F, C), Puntos),
+    C1 is C + 1,
+    generarCasillasDesde(F, C1, N, Puntos, Resto).
 
 
+/* porCasillasPrimero(+Casillas, +Puntos, -Caminos) ← dada la lista de puntos libres Casillas y la lista de valores iniciales Puntos,
+Inicia la construcción del camino para el primer par de puntos, colocando el origen al inicio de la lista Caminos y delega la busqueda de
+los puntos intermedios a porCasillas. Además asegura la restricción de que todas las casillas esten ocupadas al recorrer todos los caminos.*/
 
-% N Tamanio del tablero
-% Puntos Lista de Puntos iniciales
-% Puntos Lista de Puntos iniciales sobre la que se realiza recursion
-% Caminos Acumulador de puntos que formaran un camino
-% C El camino a retornar
-% Acumulador de Puntos visitados
-% Acumulador de tamanio de Lista de puntos
-numberLink_acc(N,Puntos,[(Ini,Fin)|Ps], Caminos, C, Visitados, VLength) :-
-  select((Ini,Fin), Puntos, PuntosAux), toListPuntos(PuntosAux,PuntosList,_), %Puntos iniciales menos Punto actual como Visitados
-  caminos(N,PuntosList,Fin,Ini, Visitados, VLength, C1, NuevosVisitados, NVLength), 
-  numberLink_acc(N, Puntos, Ps, [C1|Caminos], C, NuevosVisitados, NVLength).
+porCasillasPrimero([], [], []).
 
-% Paso base => Se cumple si Visite todo el tablero.
-numberLink_acc(N,_,[],Caminos,Caminos,_,VLength) :- VLength is (N*N).
-
-% N Tamanio del tablero
-% Puntos Lista de Puntos iniciales sin el punto actual(Ini,Fin)
-% Fin Punto final
-% Ini Punto Inicial
-% Visitados Lista de puntos ya visitados
-% VLength Largo de lista de puntos visitados
-% NuevoVisitados Nueva lista de visitados luego de encontrado un camino
-% NVLength largo de nueva lista de visitados
-caminos(N, Puntos, Fin, Ini, Visitados, VLength, C, NuevoVisitados, NVLength) :- 
-  VLength1 is VLength + 1,
-  caminos_acc(N, Puntos, Fin, Ini, [Fin|Visitados], VLength1, C, [Fin], NuevoVisitados, NVLength).
+porCasillasPrimero(Casillas, [((IX, IY), (FX, FY)) | Puntos], [[(IX, IY) | Resto] | Caminos]) :-
+    porCasillas(Casillas, [((IX, IY), (FX, FY)) | Puntos], [Resto | Caminos]).
 
 
-caminos_acc(N,Puntos,I,F,Visitados, VLength, C, Cacc, NuevoVisitados, NVLength) :-  
-  generar_adyacente_no_visitado(N,I,Visitados,Puntos, P),
-  VLength1 is VLength + 1,
-  caminos_acc(N,Puntos,P,F,[P|Visitados],VLength1, C, [P|Cacc], NuevoVisitados, NVLength).
+/*porCasillas(+Casillas, +Puntos, -Caminos) ← Construye el camino intermedio hasta el final.*/
 
-caminos_acc(_,_,X,X,Visitados,VLength,Cacc,Cacc,Visitados,VLength).
+porCasillas(Casillas, [((IX, IY), (FX, FY)) | Puntos], [[(FX, FY)] | Caminos]) :-
+    estanPegados((IX, IY), (FX, FY)),
+    porCasillasPrimero(Casillas, Puntos, Caminos).
 
-% N Tamanio del tablero
-% P Punto para el que se retornar los adyacentes
-% Lista de puntos visitados
-% Puntos Lista de puntos Iniciales menos los puntos actuales (Ini, Fin)
-% (I,J) Punto adyacente generado
-generar_adyacente_no_visitado(N, P, Visitados, Puntos, (I,J)) :-
-  N1 is N,
-  between(1,N1,I),
-  between(1,N1,J),
-  adyacente(P,(I,J),N),
-  not_member_pares((I,J), Puntos),
-  not_member_pares((I,J), Visitados).
-  
-
-% Predicado para determinar si dos puntos son adyacentes en un tablero de
-% tamanio N.
-adyacente((I1, J1), (I1, J2), N) :- J2 is J1 + 1, J1 < N.
-adyacente((I1, J1), (I1, J2), N) :- J2 is J1 - 1, J1 > 1.
-adyacente((I1, J1), (I2, J1), N) :- I2 is I1 + 1, I1 < N.
-adyacente((I1, J1), (I2, J1), N) :- I2 is I1 - 1, I1 > 1.
+porCasillas(Casillas, [((IX, IY), (FX, FY)) | Puntos], [[(CX, CY) | Resto] | Caminos]) :-
+    seleccionar((CX, CY), Casillas, Casillas2),
+    estanPegados((CX, CY), (IX, IY)),
+    porCasillas(Casillas2, [((CX, CY), (FX, FY)) | Puntos], [Resto | Caminos]).
 
 
-/* toListPuntos(+Puntos,L,Length) <- Devuelve en L una lista que contiene las posiciones 
-  en el tablero definidas en Puntos y en Length el largo de L. 
-  Ejemplo:
-  ?- tiListPuntos([((1,1),(1,2)),((2,1),(2,2))] L, Length).
-    L = [(2,1),(2,2),(1,1),(2,2)], 4)
-    Length = 4.
-*/
-toListPuntos([],[],0).
-toListPuntos([(I,J)|Puntos], [I,J|L], Length1) :-  
-  toListPuntos(Puntos, L, Length),
-  Length1 is Length + 1. 
+/*noPertenece(+X, +L) ← Devuelve true si X no pertenece a la lista L.*/
 
-/* not_member(X,L) <- true si X no esta en L, siendo X un par y L una lista de pares 
-Ejemplo:
-?- not_member((1,1),[(1,2),(1,3),(1,4),(1,5)]).
-true.
-?- not_member((1,2),[1,2),(1,3),(1,4),(1,5)]).
-false.
-*/
-
-not_member_pares(_,[]).
-not_member_pares((I,J),[(Li,Lj)|Ls]) :- 
-    I =\= Li, J =\= Lj, 
-    not_member_pares((I,J),Ls). 
-not_member_pares((I,J),[(I,Lj)|Ls]) :- 
-    J =\= Lj, 
-    not_member_pares((I,J),Ls). 
-not_member_pares((I,J),[(Li,J)|Ls]) :- 
-    I =\= Li, 
-    not_member_pares((I,J),Ls). 
+noPertenece(_, []).
+noPertenece(X, [Y | Ys]) :-
+    X \= Y,
+    noPertenece(X, Ys).
 
 
+/*perteneceAPunto(+X, +Puntos) ← Devuelve true si X es un punto inicial o final de algún par de Puntos.*/
 
-    
-
-
-
-
-
-
+perteneceAPunto(X, [(X, _) | _]).
+perteneceAPunto(X, [(_, X) | _]).
+perteneceAPunto(X, [_ | Ys]) :-
+    perteneceAPunto(X, Ys).
 
 
+/*noPerteneceAPunto(+X, +Puntos) ← Devuelve true si X no es ni punto inicial ni punto final
+de ningún par de Puntos.*/
+
+noPerteneceAPunto(_, []).
+noPerteneceAPunto(X, [(H, F) | Ys]) :-
+    X \= H,
+    X \= F,
+    noPerteneceAPunto(X, Ys).
 
 
+/*estanPegados(+P1, +P2) ← Devuelve true si P1 y P2 son adyacentes.*/
+
+estanPegados((X1, Y1), (X2, Y2)) :-
+    X1 =:= X2,
+    D is Y1 - Y2,
+    (D =:= 1 ; D =:= -1).
+
+estanPegados((X1, Y1), (X2, Y2)) :-
+    Y1 =:= Y2,
+    D is X1 - X2,
+    (D =:= 1 ; D =:= -1).
